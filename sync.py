3c7d711fdf448a5a0d0660aee93b63b045e712c1
Harrison Beachey <hbeachey@nd.edu>
2017-08-10 14:59:22 -0400

    update contentful sync to be faster, and run on lambda

--- sync.py
+++ sync.py
@@ -1,11 +1,55 @@
+# This allows gevent to be imported from the ./lib diretory
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+import sys
+sys.path.append(dir_path + "/lib/")
+
+import gevent.monkey
+gevent.monkey.patch_all()
+import gevent
+
 from hesburgh import heslog, hesutil
 import json
 import re
 import shared
 import urllib2
+import time
+
+# The contentful calls per second is 10, but that's magically gone away, so do 50 at a time
+concurentItems = 50
+
+def updateItem(item):
+  currentTitle = item.get("fields", {}).get("title", {}).get("en-US")
+  currentDesc = item.get("fields", {}).get("description", {}).get("en-US")
+  currentPurl = item.get("fields", {}).get("purl", {}).get("en-US")
+
+  alephNumber = item.get("fields", {}).get("alephSystemNumber", {}).get("en-US")
+  sysId = item.get('sys', {}).get('id')
+
+  if not alephNumber:
+    heslog.warn("No Aleph number for %s" % (sysId))
+    return None
+
+  alephItem = shared.getAleph(alephNumber)
+  # heslog.debug("number: %s, title: %s" % (alephNumber, currentTitle))
+  # heslog.debug(json.dumps(alephItem))
+
+  if (currentDesc != alephItem.get("description")
+      or currentPurl != alephItem.get("purl")):
+    alephItem["systemNumber"] = alephNumber
+    alephItem["name"] = currentTitle
+
+    version = item.get("sys", {}).get("version", 1)
+
+    updated = shared.updateContentful(sysId, version, alephItem)
+    if not updated:
+      return
+    shared.publishContentful(sysId, updated.get("sys", {}).get("version", 1))
+  else:
+    heslog.debug("Item is the same, not updating")
+
 
 def run(event, context):
-  heslog.setContext({})
   heslog.addLambdaContext(event, context)
 
   start = 0
@@ -44,30 +88,26 @@ def run(event, context):
 
   heslog.info("Updating %s items" % len(items))
 
-  for item in items:
-    currentTitle = item.get("fields", {}).get("title", {}).get("en-US")
-    currentDesc = item.get("fields", {}).get("description", {}).get("en-US")
-    currentPurl = item.get("fields", {}).get("purl", {}).get("en-US")
+  start = 0
 
-    alephNumber = item.get("fields", {}).get("alephSystemNumber", {}).get("en-US")
-    heslog.addContext(aleph=alephNumber)
-    if not alephNumber:
-      heslog.error("No Aleph number for %s" % json.dumps(item))
-      continue
+  timer = hesutil.Timer()
+  while len(items) > 0:
+    threads = []
+    use = items[:concurentItems]
+    items = items[concurentItems:]
 
-    alephItem = shared.getAleph(alephNumber)
-    heslog.debug("number: %s, title: %s" % (alephNumber, currentTitle))
-    heslog.debug(json.dumps(alephItem))
+    heslog.info("Processing items %s through %s" % (start, start + len(use)))
+    start += concurentItems
+    timer.start()
+    for item in use:
+      threads.append(gevent.spawn(updateItem, item))
 
-    sysId = item.get('sys', {}).get('id')
+    gevent.joinall(threads)
+    dt = timer.end()
+    if dt < 1:
+      # sleep to make it less likely we hit the contentful api limit
+      time.sleep(1 - dt)
 
-    if (currentDesc != alephItem.get("description")
-        or currentPurl != alephItem.get("purl")):
-      alephItem["systemNumber"] = alephNumber
-      alephItem["name"] = currentTitle
-      shared.updateContentful(sysId, item.get("sys", {}).get("version", 1), alephItem)
-    else:
-      heslog.info("Item is the same, not updating")
 
   heslog.info("Returning 200 OK")
   return { "statusCode": 200, "body": "" }
